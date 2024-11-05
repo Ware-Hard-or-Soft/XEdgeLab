221efa442276e50e5873f2d66e6734a09a2b7520
David Zheng <153074367+Ware-Hard-or-Soft@users.noreply.github.com>
2024-11-05 01:05:07 -0800

    Create model_conversion.py

---
+++ model_training/model_conversion.py
@@ -0,0 +1,15 @@
+import onnx
+import tensorflow as tf
+from onnx_tf.backend import prepare
+import onnxmltools
+
+onnx_model = onnxmltools.convert_xgboost(model, target_opset=10)
+tf_rep = prepare(onnx_model)
+tf_rep.export_graph("tf_model")
+
+converter = tf.lite.TFLiteConverter.from_saved_model("tf_model")
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+tflite_model = converter.convert()
+
+with open("model_training/oxygen_model.tflite", "wb") as f:
+    f.write(tflite_model)
