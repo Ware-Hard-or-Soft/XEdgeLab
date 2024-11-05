5e2fe53df878b57970fbbe1ec29d2138c99dd6c2
David Zheng <153074367+Ware-Hard-or-Soft@users.noreply.github.com>
2024-11-05 00:59:38 -0800

    Update and rename train_xgb to train_xgb.py

--- model_training/train_xgb.py
+++ model_training/train_xgb.py
@@ -21,7 +21,6 @@ data.dropna(inplace=True)
 X = data.drop(columns=['oxygen', 'pressure', 'humidity', 'temperature'])
 y = data['oxygen']
 
-# Split data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Initialize and train XGBoost model
